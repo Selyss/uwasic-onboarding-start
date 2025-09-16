f1b56b7aa4e0545da6154c58c4cf8e665ad07a78
Selyss <99344963+Selyss@users.noreply.github.com>
2025-09-16 13:44:58 -0400

    fix syntax

--- src/project.v
+++ src/project.v
@@ -25,7 +25,7 @@ module tt_um_uwasic_onboarding_selyss (
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
 
-  // Instantiate the SPI mdule
+  // Instantiate the SPI module
   spi_peripheral spi_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
@@ -36,8 +36,8 @@ module tt_um_uwasic_onboarding_selyss (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
-  )
+    .pwm_duty_cycle(pwm_duty_cycle)
+  );
 
   // Instantiate the PWM module
   pwm_peripheral pwm_peripheral_inst (
